5e6d0beb7fed79cf451e6a9168e26d682e77bd06
relredz <narytosasha@gmail.com>
2024-12-19 18:01:36 +0300

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://darkscripts.space/scripts/6a442b63-5908-47a5-a10e-016660882344_815896821812690946.lua',true))()                              
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
